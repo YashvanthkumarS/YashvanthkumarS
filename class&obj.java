81faa9c10b6955fc1263c3a25c535b4d1f37fa02
YashvanthkumarS <sekaryashvanth@gmail.com>
2024-05-29 12:01:29 +0530

    Create class&obj.java

---
+++ class&obj.java
@@ -0,0 +1,18 @@
+class A{
+	int c=30;
+	static int d=40;
+	public static void main(String[] args){
+	int a=10;
+	int b=10;
+	A yash = new A();
+	System.out.println(a*b);
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(yash.c);
+	System.out.println(d);
+	
+	
+	
+	
+}
+}
