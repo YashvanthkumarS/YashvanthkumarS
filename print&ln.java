97db643605c851c7ff49676df38e984eb5657daf
YashvanthkumarS <sekaryashvanth@gmail.com>
2024-05-29 12:02:24 +0530

    Create print&ln.java

---
+++ print&ln.java
@@ -0,0 +1,12 @@
+class Yash{
+	public static void main(String[] args){
+		System.out.print("Hai");
+		System.out.println("Myself");
+		System.out.print("Yashvanth Kumar");
+		System.out.println("I was born and Raised in");
+		System.out.println("Chennai");
+		System.out.print("End");
+		System.out.println("Here");
+		System.out.print("And");
+	}
+}
